15f4da922b4a02d84b7ffe19e1c1faf4314f76e3
Joshua Swartzell <50595685+Joshuas193@users.noreply.github.com>
2021-01-18 14:54:49 -0600

    Finishing up a few items. Program is complete.

--- website/app.js
+++ website/app.js
@@ -1,26 +1,30 @@
 // Global Variables
 const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?zip=';
 const apiKey = '&units=imperial&appid=c3674100cc3382bf47fccf8637a3e903';
+const dateNow = new Date().toLocaleDateString();
 
-//Adding an event listener to the generate button
+// Adding an event listener to the generate button
 document.querySelector('#generate').addEventListener('click', createPost);
 
-//Callback function to create a post
+// Callback function to create a post
 function createPost() {
-  const feelings = document.querySelector('#feelings').value;
+  // Local variables
   const zip = document.querySelector('#zip').value;
-  const dateNow = new Date().toLocaleDateString();
+  const feelings = document.querySelector('#feelings').value;
+  // Calling function to retrieve weather data
   getWeather(baseUrl, zip, apiKey)
   // Chaining promises
   .then(function(data) {
+    // calling funtion to POST data to server
     postData('/addWeather', {date: dateNow, weather: data.main.temp, content: feelings});
+    // Calling the Update UI function
     updateUi();
   })
 }
 
-//async fetch of weather data
+// Async fetch of weather data
 const getWeather = async (baseUrl, zip, apiKey) => {
-  const res = await fetch(baseUrl+zip+apiKey)
+  const res = await fetch(baseUrl+zip+apiKey) //creating the API Key dynamically with user input
   try {
     const data = await res.json();
     console.log(data);
@@ -30,7 +34,7 @@ const getWeather = async (baseUrl, zip, apiKey) => {
   }
 }
 
-//Setting up POST route
+// Async setting up POST
 const postData = async ( url = '', data = {}) => {
   const response = await fetch(url, {
     method: 'POST',
@@ -49,6 +53,7 @@ const postData = async ( url = '', data = {}) => {
   }
 }
 
+// Async function to update the UI with the information from the user and from the API
 const updateUi = async () => {
   const response = await fetch('/all');
   try {
