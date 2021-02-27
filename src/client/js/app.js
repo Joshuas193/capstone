fad6903217bc779b2866ba8cc902fe651bd02a81
Joshua Swartzell <50595685+Joshuas193@users.noreply.github.com>
2021-02-26 23:00:58 -0600

    Added Weather API. Throwing an error

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,37 +1,42 @@
-const city = document.querySelector('#city').value;
-const encodedCity = encodeURIComponent(city);
-const baseUrl = `http://api.geonames.org/searchJSON?q=`;
-const options = `${encodedCity}&maxRows=1&username=joshuas1411`;
-//const baseWeatherUrl = `https://api.weatherbit.io/v2.0/forecast/daily?`;
-//const weatherOptions = `units=I&days=7&lat=${lat}&lng=${lng}&API_KEY=9bd6b8d321a64509ad2a9dbc04fbebfc`;
-
-// Callback function to create a post
+// Callback function to create a post from Index.js
 function createPost() {
-  // Local variables
+  const city = document.querySelector('#city').value;
+  const encodedCity = encodeURIComponent(city);
   console.log(encodedCity);
-  // Calling function to retrieve weather data
-  getData(baseUrl, options).then(
-    getWeather()
-  )
-  // Chaining promises
-  .then(function(data) {
+  const baseUrl = `http://api.geonames.org/searchJSON?q=`;
+  const options = `${encodedCity}&maxRows=1&username=joshuas1411`;
+  const baseWeatherUrl = `https://api.weatherbit.io/v2.0/forecast/daily?`;
+  const weatherOptions = `units=I&days=7&lat=28.53834&lon=-81.37924&key=9bd6b8d321a64509ad2a9dbc04fbebfc`;
+  // Calling function to retrieve geo data
+  getData(baseUrl, options)
+  .then(function(baseWeatherUrl, weatherOptions) {
+    // Calling function for calling weather data
+    getWeather(baseWeatherUrl, weatherOptions)
+    .then(function(data) {
     // calling funtion to POST data to server
     postData('http://localhost:3000/addData', { lat: data.geonames[0].lat, lng: data.geonames[0].lng, country: data.geonames[0].countryName });
     // Calling the Update UI function
     updateUi();
-  })
-}
+    })}
+  )};
 
-// Async fetch of weather data
+// Async fetch of geographic data
 const getData = async (baseUrl, options) => {
-  const res = await fetch(baseUrl+options) //creating the API Key dynamically with user input
-  try {
-    const data = await res.json();
-    console.log(data);
-    return data;
+  const res = await fetch(baseUrl+options); //creating the API Key dynamically with user input
+  try{
+  const data = await res.json();
+  console.log(data)
+  return data;
   } catch(error) {
-    console.log('error', error);
+    console.log('error', error)
   }
+};
+
+const getWeather = async (baseWeatherUrl, weatherOptions) => {
+  const response = await fetch(baseWeatherUrl+weatherOptions);
+  const weatherData = await response.json();
+  console.log(weatherData);
+  return weatherData;
 }
 
 // Async setting up POST
